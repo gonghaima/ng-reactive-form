f6eec624d12158e37aa3ee322a6802a90ae86788
Steven <gonghaima@hotmail.com>
2022-12-05 20:51:07 +1100

    add json-form.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JsonFormComponent } from './json-form/json-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JsonFormComponent
   ],
   imports: [
     BrowserModule,
